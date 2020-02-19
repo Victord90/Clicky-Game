756df41eed8b585bb74d4a08e35342edd3f5e42e
Victor Dominguez <vd071090@gmail.com>
2020-02-19 02:56:24 -0500

    added components folders/some js files

---
+++ my-app/src/components/navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navbar = (props) => {
+    return (
+        <nav>
+            <h1>The Office Click Game!</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
