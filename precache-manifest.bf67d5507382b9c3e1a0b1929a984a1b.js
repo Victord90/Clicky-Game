34d095276d8195f91b4febdb3a726e1d6f732154
Victor Dominguez <vd071090@gmail.com>
2020-02-25 19:01:17 -0500

    Updates

---
+++ precache-manifest.bf67d5507382b9c3e1a0b1929a984a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94a2d8b287856c3cf2d52cefc6c076a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "064e4ea7bc0845057ef6",
+    "url": "/Clicky-Game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b28977e3b6df7765ab2",
+    "url": "/Clicky-Game/static/js/2.f0a6a870.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.f0a6a870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064e4ea7bc0845057ef6",
+    "url": "/Clicky-Game/static/js/main.a59e86e6.chunk.js"
+  },
+  {
+    "revision": "0c44dba98bd5877c6a80",
+    "url": "/Clicky-Game/static/js/runtime-main.297251f5.js"
+  }
+]);
