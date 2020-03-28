d2cca7af153f004b29f92ba9f3aab16e928fcc79
Victor Dominguez <vd071090@gmail.com>
2020-03-28 01:17:06 -0400

    changed font size

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import style from "./style.css";
 
-
-const Navbar = (props) => {
-    return (
-        <nav>
-            <h1>The Office Click Game!</h1>
-            <p>{props.message}</p>
-            <span>Current Score: {props.score} || Top Score: {props.topScore}</span>
-
-        </nav>
-    )
-}
+const Navbar = props => {
+  return (
+    <nav>
+      <h1>The Office Click Game!</h1>
+      <p>{props.message}</p>
+      <span>
+        Current Score: {props.score} || Top Score: {props.topScore}
+      </span>
+    </nav>
+  );
+};
 
 export default Navbar;
