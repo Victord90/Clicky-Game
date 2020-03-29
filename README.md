b0c8e708f3b100eb51903cc33e7bfcd586f7f21c
Victor Dominguez <vd071090@gmail.com>
2020-03-29 00:02:21 -0400

    added read me

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Clicky-Game
+
+## Overview
+
+Here we have a fun memory game with The Office theme. The goal is to click on each character only once. Click all twelve characters to win, slip up and you will have to start again!
+
+## Technologies
+
+- HTML
+- CSS
+- Bootstrap
+- Javascript
+- React.js
+
+### Clicky Game Link
+
+https://victord90.github.io/Clicky-Game/
--- build/precache-manifest.e387e7c974f2f4244bd3fd00b6247a86.js
+++ build/precache-manifest.e387e7c974f2f4244bd3fd00b6247a86.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b80d0905ab7aa25546d1f14eea592ab",
+    "revision": "1c6d769ee8e6d83d139095d37290a070",
     "url": "/Clicky-Game/index.html"
   },
   {
-    "revision": "5353bd06b4de1969d6e0",
-    "url": "/Clicky-Game/static/css/main.92169f85.chunk.css"
+    "revision": "922353a0ae13f0805534",
+    "url": "/Clicky-Game/static/css/main.14ce421a.chunk.css"
   },
   {
     "revision": "2b28977e3b6df7765ab2",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Clicky-Game/static/js/2.f0a6a870.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5353bd06b4de1969d6e0",
-    "url": "/Clicky-Game/static/js/main.27882be4.chunk.js"
+    "revision": "922353a0ae13f0805534",
+    "url": "/Clicky-Game/static/js/main.56f51cab.chunk.js"
   },
   {
     "revision": "0c44dba98bd5877c6a80",
