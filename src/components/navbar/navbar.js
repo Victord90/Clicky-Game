0468f3bfce6f21747bad9a4585d9669e7d8bc247
Victor Dominguez <vd071090@gmail.com>
2020-03-28 22:31:35 -0400

    updated navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,6 +5,34 @@ function Nav() {
   return (
     <nav class="navbar navbar-dark bg-dark">
       <h2>Memory Game</h2>
+      <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-dark p-4">
+            <a href="https://github.com/Victord90/" target="blank_">
+              <h5 class="text-white h4">Git Hub</h5>
+            </a>
+            <a
+              href="https://linkedin.com/in/victor-dominguez-b444b7191"
+              target="blank_"
+            >
+              <h5 class="text-white h4">LinkedIn</h5>
+            </a>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
     </nav>
   );
 }
