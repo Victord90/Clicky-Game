644aed8a38923e6982e378db95205f58c31da3b6
Victor Dominguez <vd071090@gmail.com>
2020-03-29 00:44:54 -0400

    added styling to header section

--- build/precache-manifest.16f9fbccc48df24528628fdf9d5f3471.js
+++ build/precache-manifest.16f9fbccc48df24528628fdf9d5f3471.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c6d769ee8e6d83d139095d37290a070",
+    "revision": "eaab761e73b9a9ece2d30b61881d81f0",
     "url": "/Clicky-Game/index.html"
   },
   {
-    "revision": "922353a0ae13f0805534",
-    "url": "/Clicky-Game/static/css/main.14ce421a.chunk.css"
+    "revision": "91c80f3c1f31e8d5f2b0",
+    "url": "/Clicky-Game/static/css/main.578f80f6.chunk.css"
   },
   {
     "revision": "2b28977e3b6df7765ab2",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Clicky-Game/static/js/2.f0a6a870.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "922353a0ae13f0805534",
+    "revision": "91c80f3c1f31e8d5f2b0",
     "url": "/Clicky-Game/static/js/main.56f51cab.chunk.js"
   },
   {
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import style from "./style.css";
 
 const Header = props => {
   return (
     <div>
       <h1>The Office Click Game!</h1>
       <p>{props.message}</p>
-      <span>
+      <span id="score">
         Current Score: {props.score} || Top Score: {props.topScore}
       </span>
     </div>
